b656bf87dd9404005a7ec330eb99f759e715b5a4
Manish Kumar <mail2manish15@gmail.com>
2018-03-31 01:15:51 +0530

    Added new Arg to puppeteer for chromium

--- services/puppeteer.js
+++ services/puppeteer.js
@@ -3,15 +3,17 @@ var fs = require('fs');
 const devices = require('puppeteer/DeviceDescriptors');
 
 var service = {
-    screenshot: async(url = null, fullPage=true, device=null) => {
+    screenshot: async(url = null, fullPage = true, device = null) => {
         if (!url || !url.length) {
             return new Error('No url present');
         }
-        const browser = await puppeteer.launch();
+        const browser = await puppeteer.launch({
+            args: ['--no-sandbox', '--disable-setuid-sandbox']
+        });
         const page = await browser.newPage();
         await page.goto(url);
 
-        var file_path = __dirname+`/screenshot-${(new Date()).getTime()}-full.png`;
+        var file_path = __dirname + `/screenshot-${(new Date()).getTime()}-full.png`;
 
         await page.screenshot({
             path: file_path,
@@ -22,12 +24,12 @@ var service = {
         return file_path;
 
     },
-    devices: async()=>{
+    devices: async() => {
         // console.log(devices);
         var device_map = [];
-        for(var key in devices){
+        for (var key in devices) {
             console.log(key, isNaN(key));
-            if(isNaN(key)){
+            if (isNaN(key)) {
                 device_map.push(key);
             }
         }
